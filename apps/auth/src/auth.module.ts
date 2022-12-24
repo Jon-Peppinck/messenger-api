5ad4d306b47c6f825ad91c94ac34b7a2ea97073f
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-12-24 14:46:07 -0300

    add[Auth]: provide repository and services to Nest Injector

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,4 +1,4 @@
-import { UserEntity } from './user.entity';
+import { UserEntity } from '../../../libs/shared/src/entities/user.entity';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,7 +7,8 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 import { PostgresDBModule } from '@app/shared/postgresdb.module';
-import { SharedModule } from '@app/shared';
+import { SharedModule, SharedService } from '@app/shared';
+import { UsersRepository } from '@app/shared/repositories/users.repository';
 
 @Module({
   imports: [
@@ -22,6 +23,19 @@ import { SharedModule } from '@app/shared';
     TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    {
+      provide: 'AuthServiceInterface',
+      useClass: AuthService,
+    },
+    {
+      provide: 'UsersRepositoryInterface',
+      useClass: UsersRepository,
+    },
+    {
+      provide: 'SharedServiceInterface',
+      useClass: SharedService,
+    },
+  ],
 })
 export class AuthModule {}
